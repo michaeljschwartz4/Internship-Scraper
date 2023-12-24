e1048d8909948ec1442c940fba8b1aeb4aca7752
michaeljschwartz4 <129219066+michaeljschwartz4@users.noreply.github.com>
2023-12-23 16:53:30 -0800

    Update PositionOpportunity.java
    PositionOpportunity data is now based on user input rather than web scraping.

--- PositionOpportunity.java
+++ PositionOpportunity.java
@@ -1,26 +1,52 @@
 package com.internship;
 
 /*
- * Creates an object of type PositionOpportunity when the scraper comes across a new job
+ * Creates an object of type PositionOpportunity based on input from the user
  */
-public class PositionOpportunity {
+public class PotentialJob {
     private String dueDate, payRange, jobLink, jobTitle, company, location;
     private boolean applied;
 
 
     /*
      * Constructor for PositionOpportunity
-     * 
-     * All inputs are values scraped from the internet
      */
-    public PositionOpportunity(String dueDate, String payRange, String jobLink, String jobTitle, String company, String location, boolean applied) {
+    public PotentialJob(String jobTitle, String company, String compensation, String dueDate, String location, String jobLink) {
         this.dueDate = dueDate;
-        this.payRange = payRange;
+        this.payRange = compensation;
         this.jobLink = jobLink;
         this.jobTitle = jobTitle;
         this.company = company;
         this.location = location;
         this.applied = false;
+    }
+
+    public String getDate() {
+        return dueDate;
+    }
+
+    public String getPay() {
+        return payRange;
+    }
+
+    public String getLink() {
+        return jobLink;
+    }
+
+    public String getTitle() {
+        return jobTitle;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public boolean getStatus() {
+        return applied;
     }
 
     public String toString() {
